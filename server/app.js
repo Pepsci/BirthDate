e46443598ab3503aefcd1987458d7d15a3f2468f
Pepsci <jossfilippi@gmail.com>
2024-11-29 18:03:50 +0100

    *

--- server/app.js
+++ server/app.js
@@ -16,42 +16,37 @@ const app = express();
 
 console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 
-// CORS configuration
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Fallback to localhost during development
+    origin: process.env.FRONTEND_URL,
   })
 );
 
-// Middlewares
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "dist"))); // Modifier pour utiliser le dossier 'dist'
 
-// Static files for production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build"))); // Update if the folder name is different
-}
+app.use("/", authRouter);
+app.use("/users", usersRouter);
+app.use("/date", dateRouter);
 
-// API routes
-app.use("/api/auth", authRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/date", dateRouter);
-
-// 404 Middleware for API routes
 app.use("/api/*", (req, res, next) => {
-  const error = new Error("Resource not found.");
+  const error = new Error("Ressource not found.");
   error.status = 404;
   next(error);
 });
 
-// React fallback route for production
 if (process.env.NODE_ENV === "production") {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html")); // Modifier pour utiliser 'dist/index.html'
   });
 }
+
+app.listen(4000, () => {
+  console.log("HTTP Server running on port 4000");
+});
 
 module.exports = app;
