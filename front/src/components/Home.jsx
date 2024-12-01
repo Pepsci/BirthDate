d939c66774432fa975f4127386cf4ce1cad87ff8
Pepsci <jossfilippi@gmail.com>
2024-12-01 22:16:21 +0100

    noel en cours

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { AuthContext } from "../context/auth.context";
 import DateList from "./dashboard/DateList";
 import "./dashboard/css/homePage.css";
+import guirlande from "../components/dashboard/images/guirlande.png";
 
 const Home = () => {
   const { logOut, isLoggedIn, currentUser } = useContext(AuthContext);
@@ -9,26 +10,37 @@ const Home = () => {
 
   return (
     <div className="homePageRoot">
-      <div className="headerApp homePageHeader">
-        <h1 className="titleFont titleFontSize">BirthDate</h1>
-        {isLoggedIn && (
-          <div className="homePageUser">
-            <div className="homePageCurrentUser">
-              <img
-                src={`https://api.dicebear.com/8.x/bottts/svg?seed=${currentUser.surname}`}
-                alt="avatar"
-                className="avatar"
-              />
-              <div className="homePageCurrentUserName">
-                {currentUser && currentUser.name}
+      <div className="homeTest">
+        <div className="headerApp homePageHeader">
+          <h1 className="titleFont titleFontSize">BirthDate</h1>
+          {isLoggedIn && (
+            <div className="homePageUser">
+              <div className="homePageCurrentUser">
+                <img
+                  src={`https://api.dicebear.com/8.x/bottts/svg?seed=${currentUser.surname}`}
+                  alt="avatar"
+                  className="avatar"
+                />
+                <div className="homePageCurrentUserName">
+                  {currentUser && currentUser.name}
+                </div>
               </div>
+              <button className="bntLogout" onClick={logOut}>
+                LogOut
+              </button>
             </div>
-            <button className="bntLogout" onClick={logOut}>
-              LogOut
-            </button>
-          </div>
-        )}
+          )}
+        </div>
+        {/* <div className="christmas-lights">
+          {" "}
+          <img
+            src={guirlande}
+            alt="Guirlande lumineuse de Noël"
+            className="guirlandeDeNoel"
+          />{" "}
+        </div> */}
       </div>
+
       {isLoggedIn && <DateList date={date} />}
     </div>
   );
