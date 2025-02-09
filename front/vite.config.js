e80be225a45fde108424dc8526ace37ef45163e0
Pepsci <jossfilippi@gmail.com>
2025-02-09 19:40:40 +0100

    debug https

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "localhost",
-    port: 5173,
+  build: {
+    outDir: "dist",
   },
 });
