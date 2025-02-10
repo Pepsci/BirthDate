772d93e4152dff7c6874de4a6e43c14d7d2b15ff
Pepsci <jossfilippi@gmail.com>
2025-02-11 00:34:44 +0100

    debug https

--- front/src/components/connect/Login.jsx
+++ front/src/components/connect/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     event.preventDefault();
 
     try {
-      const response = await apiHandler.post("/login", {
+      const response = await apiHandler.post("/auth/login", {
         email: user.email,
         password: user.password,
       });
