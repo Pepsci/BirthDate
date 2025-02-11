aa8925e089aa1293c2b10dc73d71331d77026ca1
Pepsci <jossfilippi@gmail.com>
2025-02-11 12:46:12 +0100

    debug https

--- front/src/api/apiHandler.jsx
+++ front/src/api/apiHandler.jsx
@@ -26,7 +26,7 @@ const apiHandler = {
 
   signup(userInfo) {
     return service
-      .post("/signup", userInfo)
+      .post("/auth/signup", userInfo)
       .then((res) => res.data)
       .catch(errorHandler);
   },
