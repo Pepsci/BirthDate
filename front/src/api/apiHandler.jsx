3d9310084a0859e74bec86b8fe5a091f99e70869
Pepsci <jossfilippi@gmail.com>
2025-02-11 12:02:33 +0100

    debug https

--- front/src/api/apiHandler.jsx
+++ front/src/api/apiHandler.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: import.meta.env.VITE_APP_BACKEND_URL,
+  baseURL: "https://birthreminder.com/",
   withCredentials: true,
 });
 
