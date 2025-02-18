e7d49581ba21ff7b0e3a86cbf268698a7319ccfa
Pepsci <jossfilippi@gmail.com>
2025-02-18 19:35:17 +0100

    correction  Empêche la pagination de revenir à la première page après un resize sur mobile

--- front/src/components/dashboard/DateList.jsx
+++ front/src/components/dashboard/DateList.jsx
@@ -15,73 +15,14 @@ const DateList = ({ onEditDate, onViewFriendProfile }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [isFormVisible, setIsFormVisible] = useState(false);
   const [isFamilyFilterActive, setIsFamilyFilterActive] = useState(false);
+  const [itemsPerPage, setItemsPerPage] = useState(
+    window.innerWidth <= 600 ? ITEMS_PER_PAGE_MOBILE : ITEMS_PER_PAGE
+  );
 
-  useEffect(() => {
-    apiHandler
-      .get(`/date?owner=${currentUser._id}`)
-      .then((dbResponse) => {
-        let filteredDates = dbResponse.data;
-        if (isFamilyFilterActive) {
-          filteredDates = dbResponse.data.filter(
-            (date) => date.family === true
-          );
-        }
-
-        const today = new Date();
-        filteredDates.sort((a, b) => {
-          const nextBirthdayA = new Date(
-            today.getFullYear(),
-            new Date(a.date).getMonth(),
-            new Date(a.date).getDate()
-          );
-          const nextBirthdayB = new Date(
-            today.getFullYear(),
-            new Date(b.date).getMonth(),
-            new Date(b.date).getDate()
-          );
-
-          // Check if the birthday is today and ensure it is displayed first
-          const isTodayBirthdayA =
-            today.getDate() === nextBirthdayA.getDate() &&
-            today.getMonth() === nextBirthdayA.getMonth();
-
-          const isTodayBirthdayB =
-            today.getDate() === nextBirthdayB.getDate() &&
-            today.getMonth() === nextBirthdayB.getMonth();
-
-          if (isTodayBirthdayA) return -1;
-          if (isTodayBirthdayB) return 1;
-
-          if (nextBirthdayA < today) {
-            nextBirthdayA.setFullYear(today.getFullYear() + 1);
-          }
-          if (nextBirthdayB < today) {
-            nextBirthdayB.setFullYear(today.getFullYear() + 1);
-          }
-          return nextBirthdayA - nextBirthdayB;
-        });
-        setDates(filteredDates);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  }, [currentUser, isFamilyFilterActive]);
-
-  if (!dates) return <p>Loading...</p>;
-
-  const toggleFormVisibility = () => {
-    setIsFormVisible(!isFormVisible);
-  };
-
-  const handleDateAdded = (newDate) => {
-    const updatedDates = [...dates, newDate];
-    sortDates(updatedDates);
-    setDates(updatedDates);
-  };
-
+  // Fonction pour trier les dates
   const sortDates = (datesArray) => {
     const today = new Date();
-    datesArray.sort((a, b) => {
+    return datesArray.sort((a, b) => {
       const nextBirthdayA = new Date(
         today.getFullYear(),
         new Date(a.date).getMonth(),
@@ -93,89 +34,60 @@ const DateList = ({ onEditDate, onViewFriendProfile }) => {
         new Date(b.date).getDate()
       );
 
-      // Check if the birthday is today and ensure it is displayed first
-      const isTodayBirthdayA =
-        today.getDate() === nextBirthdayA.getDate() &&
-        today.getMonth() === nextBirthdayA.getMonth();
-
-      const isTodayBirthdayB =
-        today.getDate() === nextBirthdayB.getDate() &&
-        today.getMonth() === nextBirthdayB.getMonth();
-
-      if (isTodayBirthdayA) return -1;
-      if (isTodayBirthdayB) return 1;
-
-      if (nextBirthdayA < today) {
+      if (nextBirthdayA < today)
         nextBirthdayA.setFullYear(today.getFullYear() + 1);
-      }
-      if (nextBirthdayB < today) {
+      if (nextBirthdayB < today)
         nextBirthdayB.setFullYear(today.getFullYear() + 1);
-      }
+
       return nextBirthdayA - nextBirthdayB;
     });
   };
 
-  const calculateAge = (birthdate) => {
-    const birthDate = new Date(birthdate);
-    const today = new Date();
+  useEffect(() => {
+    apiHandler
+      .get(`/date?owner=${currentUser._id}`)
+      .then((dbResponse) => {
+        let filteredDates = dbResponse.data;
+        if (isFamilyFilterActive) {
+          filteredDates = filteredDates.filter((date) => date.family === true);
+        }
+        setDates(sortDates(filteredDates));
+      })
+      .catch((e) => console.error(e));
+  }, [currentUser, isFamilyFilterActive]);
 
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const monthDiff = today.getMonth() - birthDate.getMonth();
-    const dayDiff = today.getDate() - birthDate.getDate();
+  useEffect(() => {
+    const handleResize = () => {
+      const newItemsPerPage =
+        window.innerWidth <= 600 ? ITEMS_PER_PAGE_MOBILE : ITEMS_PER_PAGE;
 
-    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
-      age--;
-    }
+      if (itemsPerPage !== newItemsPerPage) {
+        setItemsPerPage(newItemsPerPage);
+      }
+    };
 
-    return age;
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [itemsPerPage]);
+
+  const toggleFormVisibility = () => setIsFormVisible(!isFormVisible);
+
+  const handleDateAdded = (newDate) => {
+    const updatedDates = sortDates([...dates, newDate]);
+    setDates(updatedDates);
   };
-
-  const calculateDaysUntilBirthday = (birthdate) => {
-    const birthDate = new Date(birthdate);
-    const today = new Date();
-    const nextBirthday = new Date(
-      today.getFullYear(),
-      birthDate.getMonth(),
-      birthDate.getDate()
-    );
-    if (nextBirthday < today) {
-      nextBirthday.setFullYear(today.getFullYear() + 1);
-    }
-    const daysUntilBirthday = Math.ceil(
-      (nextBirthday - today) / (1000 * 60 * 60 * 24)
-    );
-    return daysUntilBirthday;
-  };
-
-  let itemsPerPage =
-    window.innerWidth <= 600 ? ITEMS_PER_PAGE_MOBILE : ITEMS_PER_PAGE;
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = dates.slice(indexOfFirstItem, indexOfLastItem);
-  const nextPage = () => setCurrentPage((prev) => prev + 1);
-  const prevPage = () => setCurrentPage((prev) => prev - 1);
 
-  useEffect(() => {
-    const handleResize = () => {
-      itemsPerPage =
-        window.innerWidth <= 600 ? ITEMS_PER_PAGE_MOBILE : ITEMS_PER_PAGE;
-      setCurrentPage(1);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const toggleFamilyFilter = () => {
+  const toggleFamilyFilter = () =>
     setIsFamilyFilterActive(!isFamilyFilterActive);
-  };
 
   const [viewMode, setViewMode] = useState("card");
-  const toggleViewMode = () => {
+  const toggleViewMode = () =>
     setViewMode(viewMode === "card" ? "agenda" : "card");
-  };
 
   return (
     <div className="dateList">
@@ -207,52 +119,52 @@ const DateList = ({ onEditDate, onViewFriendProfile }) => {
         <Agenda dates={dates} />
       ) : (
         <div className="birthDeck">
-          {currentItems.map((date) => {
-            return (
-              <div className="birthCard titleFont" key={date._id + "date"}>
-                <div className="birthCardName">
-                  <span className="birthCard-name">
-                    <b>{date.name}</b>
-                  </span>
-                  <span>
-                    <b>{date.surname}</b>
-                  </span>
-                  <br />
-                </div>
-                <div className="birthCardAge">
-                  <span className="age">{calculateAge(date.date)} Ans</span>
-                  <br />
-                </div>
-                <div className="birthCardDate">
-                  <span className="date">
-                    {new Date(date.date).toLocaleDateString("fr-FR")}
-                  </span>
-                  <br />
-                </div>
-                <div className="birthCard-actions birthCardCenter">
-                  <span className="daysUntilBirthday">
-                    <Countdown birthdate={date.date} />
-                  </span>
-                  <br />
-                  <button onClick={() => onEditDate(date)} className="btn-edit">
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => onViewFriendProfile(date)}
-                    className="btn-view"
-                  >
-                    View Profile
-                  </button>
-                </div>
+          {currentItems.map((date) => (
+            <div className="birthCard titleFont" key={date._id + "date"}>
+              <div className="birthCardName">
+                <span className="birthCard-name">
+                  <b>{date.name}</b>
+                </span>
+                <span>
+                  <b>{date.surname}</b>
+                </span>
               </div>
-            );
-          })}
+              <div className="birthCardAge">
+                <span className="age">
+                  {new Date().getFullYear() - new Date(date.date).getFullYear()}{" "}
+                  Ans
+                </span>
+              </div>
+              <div className="birthCardDate">
+                <span className="date">
+                  {new Date(date.date).toLocaleDateString("fr-FR")}
+                </span>
+              </div>
+              <div className="birthCard-actions birthCardCenter">
+                <span className="daysUntilBirthday">
+                  <Countdown birthdate={date.date} />
+                </span>
+                <button onClick={() => onEditDate(date)} className="btn-edit">
+                  Edit
+                </button>
+                <button
+                  onClick={() => onViewFriendProfile(date)}
+                  className="btn-view"
+                >
+                  View Profile
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       )}
 
       {viewMode === "card" && (
         <div className="pagination">
-          <button onClick={prevPage} disabled={currentPage === 1}>
+          <button
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}
+          >
             Précédent
           </button>
           {[...Array(Math.ceil(dates.length / itemsPerPage)).keys()].map(
@@ -263,7 +175,11 @@ const DateList = ({ onEditDate, onViewFriendProfile }) => {
             )
           )}
           <button
-            onClick={nextPage}
+            onClick={() =>
+              setCurrentPage((prev) =>
+                Math.min(prev + 1, Math.ceil(dates.length / itemsPerPage))
+              )
+            }
             disabled={currentPage === Math.ceil(dates.length / itemsPerPage)}
           >
             Suivant
