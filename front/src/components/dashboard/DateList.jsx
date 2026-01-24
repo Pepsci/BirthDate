eaec00655adad1164d75a5e9e03b48392a746901
Pepsci <jossfilippi@gmail.com>
2026-01-24 13:12:56 +0100

    correction

--- front/src/components/dashboard/DateList.jsx
+++ front/src/components/dashboard/DateList.jsx
@@ -5,7 +5,7 @@ import CreateDate from "./CreateDate";
 import Agenda from "./Agenda";
 import DateFilter from "./DateFilter";
 import Countdown from "./Countdown";
-import ManualMergeModal from "./ManualMergeModal";
+import ManualMergeModal from "./ManuelMergeModal";
 import "./css/dateList.css";
 import "./css/birthcard.css";
 
