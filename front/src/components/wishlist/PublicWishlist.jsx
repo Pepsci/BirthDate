b8072d591bdad7c91fb3d2de250c8a835a1bd388
Pepsci <jossfilippi@gmail.com>
2026-05-06 12:11:15 +0200

    correction import

--- front/src/components/wishlist/PublicWishlist.jsx
+++ front/src/components/wishlist/PublicWishlist.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "./css/PublicWishlist.css";
+import "./css/publicWishlist.css";
 
 const API_URL =
   window.location.hostname === "localhost"
