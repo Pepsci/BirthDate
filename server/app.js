923e5d63e7e65f82c68d1ee58361dfa8806d9697
Pepsci <jossfilippi@gmail.com>
2026-05-07 11:14:51 +0200

    stress-test

--- server/app.js
+++ server/app.js
@@ -64,6 +64,11 @@ app.use(
   }),
 );
 
+// Vérification loader.io — à ajouter une fois
+app.get("/loaderio-bcebff5a81d031074a9b23b1ec9c73b4", (req, res) => {
+  res.send("loaderio-bcebff5a81d031074a9b23b1ec9c73b4");
+});
+
 app.use((req, res, next) => {
   console.log(`📨 ${req.method} ${req.url}`);
   next();
